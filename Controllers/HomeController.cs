877b147764b2c87e206287f565ec8ac37d1f684c
Vyas <kvyas@corp.westworlds.com>
2020-01-22 20:21:10 -0600

    Add all my files

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,114 @@
+﻿using LastPassApplication.Models;
+using LastPassApplication.Models.LastPassApplication.Models.Extended;
+//using LastPassApplication.Models.Extended;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace LastPassApplication.Controllers
+{
+    public class HomeController : Controller
+    {
+        Models.LastPassDatabaseEntities1 db = new Models.LastPassDatabaseEntities1();
+
+        [HttpGet]
+        [OutputCache(Duration = 60)]
+        public ActionResult AddNewPassword()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult AddNewPassword(AddPassword addpassword)
+        {
+            //Here i have implemented the code for doing encryption of password
+            //string ePass = Helper.ComputeHash(addpassword.SitePassword, "SHA512", null);
+            //addpassword.SitePassword = ePass.ToString();
+            addpassword.SitePassword = Helper.Encryptdata(addpassword.SitePassword);
+           
+
+            db.AddPassword.Add(addpassword);
+            db.SaveChanges();
+
+            return RedirectToAction("AddNewPassword");
+        }
+
+        [HttpGet]
+        [OutputCache(Duration = 60)]
+        public ActionResult Decrypt()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Decrypt(AddPassword addPassword)
+        {
+            if(string.IsNullOrEmpty(addPassword.UserName))
+            {
+                ModelState.AddModelError("UserName", "UserName is required");
+            }
+
+            if (string.IsNullOrEmpty(addPassword.NameSite))
+            {
+                ModelState.AddModelError("NameSite", "Name Site is required");
+            }
+
+            if (ModelState.IsValid)
+            {
+                AddPassword objuser = db.AddPassword.Where(x => x.UserName == addPassword.UserName && x.NameSite == addPassword.NameSite).FirstOrDefault();
+
+
+                Session["SitePassword"] = Helper.Decryptdata(objuser.SitePassword);
+            }            
+            return View();
+        }
+
+
+
+       
+
+
+        // GET: Home
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+    
+        [HttpPost]
+        
+        public ActionResult AddPassword(AddPassword addpassword)
+        {
+            //Here i have implemented the code for doing encryption of password
+            string ePass = Helper.ComputeHash(addpassword.SitePassword, "SHA512", null);
+            addpassword.SitePassword = ePass.ToString();
+                
+            db.AddPassword.Add(addpassword);
+            db.SaveChanges();
+
+            return View();
+
+        }
+
+      
+
+        [HttpGet]
+        public ActionResult EndPage()
+        {
+
+            return View();
+        }
+
+    }
+    }
+
+
+
+
+
+
+
+
---
+++ Models/AddPasswords.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace LastPassApplication.Models
+{
+    public class AddPasswords
+    {
+
+        public int ID { get; set; }
+        public int LoginID { get; set; }
+        public string UrlName { get; set; }
+        public string NameSite { get; set; }
+        public string UserName { get; set; }
+
+        [Required(ErrorMessage = "Please Enter Password")]
+        [DataType(DataType.Password)]
+        public string SitePassword { get; set; }
+        public string Notes { get; set; }
+
+
+       
+       
+    }
+}
